d6cc71a5e667675a77e730c29822432f599ecc87
hullys31 <hullysdesire@gmail.com>
2024-01-17 17:43:40 +0100

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-export default function App() {
-  return (
+export default function App(){
+  return(
     <h1 className="text-3xl font-bold underline">
-      Hello world!
+      hello world !
     </h1>
   )
 }
